496b9eefdaa8fc0c7fa2527367b4b0909c1cfd1a
Avinash Sharma <78852355+Avinash8225048544@users.noreply.github.com>
2023-05-08 22:35:30 +0530

    Tooltip.jsx
    new changes code

--- src/Tooltip.jsx
+++ src/Tooltip.jsx
@@ -23,23 +23,23 @@ function Tooltip({ text, position, children }) {
 
   switch (position) {
     case "top":
-      tooltipStyle.top = "155px";
-      tooltipStyle.left = "53%";
+      tooltipStyle.top = "210px";
+      tooltipStyle.left = "51%";
       tooltipStyle.transform = "translateX(-50%)";
       break;
     case "bottom":
-      tooltipStyle.top = "225px";
-      tooltipStyle.left = "54%";
+      tooltipStyle.top = "285px";
+      tooltipStyle.left = "51.5%";
       tooltipStyle.transform = "translateX(-50%)";
       break;
     case "left":
-      tooltipStyle.top = "54%";
-      tooltipStyle.right = "260px";
+      tooltipStyle.top = "53%";
+      tooltipStyle.right = "420px";
       tooltipStyle.transform = "translateY(-50%)";
       break;
     case "right":
-      tooltipStyle.top = "54%";
-      tooltipStyle.left = "290px";
+      tooltipStyle.top = "53%";
+      tooltipStyle.left = "450px";
       tooltipStyle.transform = "translateY(-50%)";
       break;
     default:
